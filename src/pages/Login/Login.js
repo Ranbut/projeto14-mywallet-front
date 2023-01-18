a50970d6457fe2e202b56d2953b5fe1fd66ce6ba
Ranbut <Ranbut_Beater@protonmail.com>
2023-01-17 22:18:08 -0300

    Axios faz login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,6 +2,8 @@ import {Container, Logo, Formulario} from './styles.js'
 import { useState, useContext } from 'react';
 import { Link ,useNavigate } from "react-router-dom";
 import { UsuarioContext } from '../../contexts/UsuarioContext';
+import {URL_Login} from '../../constants/urls.js'
+import axios from 'axios';
 
 function Login(){
 
@@ -10,15 +12,24 @@ function Login(){
     const {setUsuario} = useContext(UsuarioContext);
     const navigate = useNavigate();
 
+
+    function entrar(event){
+        event.preventDefault();
+        setClicado(true);
+        const requisicao = axios.post(`${URL_Login}`, login);
+        requisicao.then((res) => {navigate("/home"); }) ;
+        requisicao.catch((res) => {alert(res.response.data.message); setClicado(false);}) ;
+    }
+
     return(
     <Container>
         <Logo>MyWallet</Logo>
-        <Formulario>
-        <input disabled={clicado} required type="email" placeholder="E-mail" value={login.email} onChange={e => setLogin({...login, email: e.target.value})}/>
-        <input disabled={clicado} required type="password" placeholder="Senha" value={login.password} onChange={e => setLogin({...login, password: e.target.value} )}/>
-        <button disabled={clicado} type="submit">
+        <Formulario onSubmit={entrar} clicado={clicado}>
+            <input disabled={clicado} required type="email" placeholder="E-mail" value={login.email} onChange={e => setLogin({...login, email: e.target.value})}/>
+            <input disabled={clicado} required type="password" placeholder="Senha" value={login.password} onChange={e => setLogin({...login, password: e.target.value} )}/>
+            <button disabled={clicado} type="submit">
             Entrar
-        </button>
+            </button>
         </Formulario>
         <Link to="/cadastro">Primeira vez? Cadastre-se!</Link>
     </Container>
